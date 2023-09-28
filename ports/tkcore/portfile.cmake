0ee6599ec7a2ed5883fbaa20e9d6fe7b36fcf26b
runner <runner@Mac-1695891315888.local>
2023-09-28 09:08:34 +0000

    Update tkcore 1.0.3

--- ports/tkcore/portfile.cmake
+++ ports/tkcore/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_download_distfile(ARCHIVE
-  URLS "https://github.com/TKCPlusProjects/tkcore/releases/download/1.0.2/tkcore-1.0.2.zip"
-  FILENAME "tkcore-1.0.2.zip"
-  SHA512 6455e67ea519ff296a666e03ed430f0888849205333393013f3d740d8997fb6628fb81ded7d5d279b7eb816200e733a708bb39b88693265d9c7b6a9db56ccd57
+  URLS "https://github.com/TKCPlusProjects/tkcore/releases/download/1.0.3/tkcore-1.0.3.zip"
+  FILENAME "tkcore-1.0.3.zip"
+  SHA512 2926add0b37f3303e356e8f0d67ff120a9cc4dcaa47805cf573e45f49f2d146be0f91b5efc376ebf49ad749654b73a14d6a83b36a8380bee8bd4405dc0682875
 )
 
 vcpkg_extract_source_archive(
@@ -13,7 +13,7 @@ vcpkg_extract_source_archive(
 vcpkg_cmake_configure(
   SOURCE_PATH "${SOURCE_PATH}"
   OPTIONS
-    -DRELEASE_VERSION=1.0.2
+    -DRELEASE_VERSION=1.0.3
 )
 
 vcpkg_cmake_install()
